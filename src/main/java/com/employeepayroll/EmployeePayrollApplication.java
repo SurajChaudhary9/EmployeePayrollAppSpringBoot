e5cec40c899bf52b2b582247301516ab219ec464
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-07 09:39:26 +0530

    Added : Welcome Message

---
+++ src/main/java/com/employeepayroll/EmployeePayrollApplication.java
@@ -0,0 +1,14 @@
+package com.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll App !!!!");
+		SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
+
+}
