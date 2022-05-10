86c1b323dd7cfd4b90a8951bc13d41e387d24eb6
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-10 12:28:05 +0530

    Added : Lombok Data Annotation For Getter Setter

--- src/main/java/com/employeepayroll/dto/ResponseDTO.java
+++ src/main/java/com/employeepayroll/dto/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.employeepayroll.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO {
     private String message;
     private Object data;
@@ -9,19 +12,4 @@ public class ResponseDTO {
         this.data=data;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
