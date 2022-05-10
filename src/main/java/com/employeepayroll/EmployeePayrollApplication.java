d71e7d0a0d228f4375ffbb6bed1cddae3fa6969b
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-10 12:39:11 +0530

    Added : Dev & Production Environment

--- src/main/java/com/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/employeepayroll/EmployeePayrollApplication.java
@@ -3,6 +3,7 @@ package com.employeepayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
@@ -10,8 +11,9 @@ public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Payroll App !!!!!");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-		log.info("employee payroll app started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
